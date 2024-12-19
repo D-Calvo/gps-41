a4a8a07f6c6e994de77dc985de0eedf47d4813d7
david.calvo.espases <david.calvo.espases@estudiantat.upc.edu>
2024-12-19 14:46:48 +0100

    CI

--- test.py
+++ test.py
@@ -3,18 +3,22 @@ import transform
 
 
 class TestStringMethods(unittest.TestCase):
+    """Classe per provar les transformacions de strings."""
 
     def test_is_upper(self):
-        sting = transform.to_upper_case("hello")
-        self.assertEqual(sting, "HELLO")
+        """Comprova si una cadena es transforma correctament a majúscules."""
+        string = transform.to_upper_case("hello")
+        self.assertEqual(string, "HELLO")
 
     def test_is_lower(self):
-        sting = transform.to_lower_case("HELLO")
-        self.assertEqual(sting, "hello")
+        """Comprova si una cadena es transforma correctament a minúscules."""
+        string = transform.to_lower_case("HELLO")
+        self.assertEqual(string, "hello")
 
     def test_is_capitalize(self):
-        sting = transform.to_capitalize("HELLO")
-        self.assertEqual(sting, "Hello")
+        """Comprova si una cadena es transforma correctament a format capitalitzat."""
+        string = transform.to_capitalize("HELLO")
+        self.assertEqual(string, "Hello")
 
 
 if __name__ == '__main__':
